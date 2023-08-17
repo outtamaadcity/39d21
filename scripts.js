c75c407bac131a710c2660b63558b2ad794f2dd9
good kid <141099144+outtamaadcity@users.noreply.github.com>
2023-08-17 19:17:04 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,18 +1,11 @@
-document.addEventListener("DOMContentLoaded", function() {
-    if (window.location.pathname.includes('index.html')) {
-        loadIndex();
-    } else if (window.location.pathname.includes('post.html')) {
-        let postId = new URLSearchParams(window.location.search).get('id');
-        loadPost(postId);
-    }
-});
-
 function loadIndex() {
     fetch('posts.json')
     .then(response => response.json())
     .then(posts => {
-        // Add posts to the post list
         let postListDiv = document.getElementById('postList');
+        let blogPostsDiv = document.getElementById('blogPosts');
+
+        // Add posts to the post list
         posts.forEach((post, index) => {
             let listItem = document.createElement('li');
             let link = document.createElement('a');
@@ -20,43 +13,20 @@ function loadIndex() {
             link.textContent = post.title;
             listItem.appendChild(link);
             postListDiv.appendChild(listItem);
+
+            // Also add the full post below
+            let postDiv = document.createElement('div');
+            let postTitle = document.createElement('h3');
+            postTitle.textContent = post.title;
+            let postContent = document.createElement('p');
+            postContent.textContent = post.content;
+            postDiv.appendChild(postTitle);
+            postDiv.appendChild(postContent);
+            blogPostsDiv.appendChild(postDiv);
         });
 
         // Display the latest post
         let latestPost = posts[posts.length - 1];
         let latestPostDiv = document.getElementById('latestPost');
         let postTitle = document.createElement('h2');
-        postTitle.textContent = latestPost.title;
-        let postContent = document.createElement('p');
-        postContent.textContent = latestPost.content;
-        latestPostDiv.appendChild(postTitle);
-        latestPostDiv.appendChild(postContent);
-
-        // Add post page buttons
-        let postPagesDiv = document.getElementById('postPages');
-        for (let i = 0; i < posts.length; i++) {
-            let button = document.createElement('button');
-            button.textContent = i + 1;
-            button.onclick = function() {
-                window.location.href = `post.html?id=${i}`;
-            };
-            postPagesDiv.appendChild(button);
-        }
-    })
-    .catch(error => {
-        console.error("Error fetching posts:", error);
-    });
-}
-
-function loadPost(postId) {
-    fetch('posts.json')
-    .then(response => response.json())
-    .then(posts => {
-        let post = posts[postId];
-        document.getElementById('postTitle').textContent = post.title;
-        document.getElementById('postContent').textContent = post.content;
-    })
-    .catch(error => {
-        console.error("Error fetching posts:", error);
-    });
-}
+        postTitle.textContent =
